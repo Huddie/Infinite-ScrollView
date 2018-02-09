935ad902b790fc9ab9bd023b325811e7a7262cc2
Ehud Adler <easports96@gmail.com>
2018-02-08 21:45:10 -0500

    Added delegate method and reformatted

--- InfScrollView/InfiniteScrollView.swift
+++ InfScrollView/InfiniteScrollView.swift
@@ -8,89 +8,132 @@
 
 import UIKit
 
-class InfiniteScrollView: UIScrollView {
-  
-  var visibleView = [UIView]()
-  var container   = UIView()
-  
-  override init(frame: CGRect) {
+enum direction
+{
+  case left
+  case right
+}
+
+
+protocol InfiniteScrollViewDataSource: class
+{
+  func infiniteItemForDirecection(_ direction: direction) -> UIView
+}
+
+
+class InfiniteScrollView: UIScrollView
+{
+  fileprivate  var visibleView         = [UIView]()
+  fileprivate  var container           = UIView()
+  internal     var InfiniteDelegate    : InfiniteScrollViewDataSource?
+
+  override init(frame: CGRect)
+  {
     super .init(frame: frame)
     self.setUp()
   }
   
-  required init?(coder aDecoder: NSCoder) {
+  required init?(coder aDecoder: NSCoder)
+  {
     super.init(coder: aDecoder)
     self.setUp()
   }
   
-  private func setUp(){
+  public func reloadLayout()
+  {
+    setUp()
+  }
+  private func setUp()
+  {
     self.contentSize = CGSize(width: self.frame.width*4, height: self.frame.height)
-    container = UIView(frame: CGRect(x: 0, y: 0, width: self.contentSize.width, height: self.contentSize.height))
+    self.container   = UIView(frame: CGRect(x: 0, y: 0,
+                                     width: self.contentSize.width, height: self.contentSize.height))
+    
+
     self.addSubview(container)
   }
   
-  private func recenterIfNecessary(){
-    let currentOffset = self.contentOffset
-    let contentWidth = self.contentSize.width
-    let centerOffsetX = (contentWidth - (self.bounds.size.width)) / 2.0
+  private func recenterIfNecessary()
+  {
+    let currentOffset      = self.contentOffset
+    let contentWidth       = self.contentSize.width
+    let centerOffsetX      = (contentWidth - (self.bounds.size.width)) / 2.0
     let distanceFromCenter = fabs((currentOffset.x) - centerOffsetX)
     
-    if (distanceFromCenter > (contentWidth / 4.0)) {
-      self.contentOffset = CGPoint(x: centerOffsetX, y:  (currentOffset.y))
+    if (distanceFromCenter > (contentWidth / 4.0))
+    {
+      self.contentOffset = CGPoint(x: centerOffsetX,
+                                   y:  (currentOffset.y))
       
       // move content by the same amount so it appears to stay still
-      for view in visibleView {
-        var theCenter = view.center
-        theCenter.x += centerOffsetX - currentOffset.x
-        view.center = theCenter
+      for view in visibleView
+      {
+        var center  = view.center
+        center.x   += centerOffsetX - currentOffset.x
+        view.center = center
       }
     }
   }
   
   // recenter content periodically to achieve impression of infinite scrolling
-  override func layoutSubviews() {
+  override func layoutSubviews()
+  {
     
     super.layoutSubviews()
     
     recenterIfNecessary()
     
-    let visBounds = self.bounds
+    let visBounds    = self.bounds
     let minVisBounds = visBounds.minX - self.frame.width
     let maxVisBounds = visBounds.maxX
-    
+  
     placeViews(min: minVisBounds, max: maxVisBounds)
     
   }
   
   // Correctly building, add to view and return for proper placement
-  private func createBuilding() -> UIView {
-    let newView = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height))
-    newView.backgroundColor = generateRandomColor()
-    container.addSubview(newView)
-    return newView
+  private func createBuilding(direction: direction) -> UIView
+  {
+    let newView    = InfiniteDelegate?.infiniteItemForDirecection(direction)
+    newView?.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height)
+    
+    if let newView = newView
+    {
+      container.addSubview(newView)
+      return newView
+    }
+    assert(true, "Cannot pass in blank view")
+    return UIView()
   }
   
   // Correctly place view on the right edge
-  private func placeNewOnRight(rightEdge: CGFloat) -> CGFloat {
-    let newView = createBuilding()
+  private func placeNewOnRight(rightEdge: CGFloat) -> CGFloat
+  {
+    
+    let newView = createBuilding(direction: .right)
     visibleView.append(newView)
-    var newframe = newView.frame
+    
+    var newframe      = newView.frame
     newframe.origin.x = rightEdge
-    newView.frame = newframe
+    newView.frame     = newframe
     
     return newframe.maxX
-    
+  
   }
   
   // Correctly place view on the left edge
-  private func placeNewOnLeft(leftEdge: CGFloat) -> CGFloat {
-    let newView = createBuilding()
+  private func placeNewOnLeft(leftEdge: CGFloat) -> CGFloat
+  {
+    
+    let newView = createBuilding(direction: .left)
     visibleView.insert(newView, at: 0)
-    var newframe = newView.frame
+    
+    var newframe      = newView.frame
     newframe.origin.x = leftEdge - newframe.size.width
-    newView.frame = newframe
+    newView.frame     = newframe
     
     return newframe.minX
+    
   }
   
   private func placeViews(min: CGFloat, max: CGFloat)
@@ -99,27 +142,22 @@ class InfiniteScrollView: UIScrollView {
     // There must be atleast 1 view in the visible array for this function to run properly.
     // Therefore, to start, we make sure that atleast one view exists
     
-    if visibleView.count < 1 {
-      _ = self.placeNewOnRight(rightEdge: min)
-    }
+    if visibleView.count < 1 { _ = self.placeNewOnRight(rightEdge: min) }
     
     // Add views that are missing on right side
-    var lastView = visibleView.last
+    var lastView           = visibleView.last
     var rightEdge: CGFloat = (lastView?.frame.maxX)!
-    while rightEdge < max {
-      rightEdge = self.placeNewOnRight(rightEdge: rightEdge)
-    }
+    while rightEdge < max { rightEdge = self.placeNewOnRight(rightEdge: rightEdge) }
     
     // Add views that are missing on left side
-    var firstView = visibleView.first
+    var firstView         = visibleView.first
     var leftEdge: CGFloat = (firstView?.frame.minX)!
-    while leftEdge > min {
-      leftEdge = self.placeNewOnLeft(leftEdge: leftEdge)
-    }
+    while leftEdge > min { leftEdge = self.placeNewOnLeft(leftEdge: leftEdge) }
     
     // Remove views that have fallen off the right edge
     lastView = visibleView.last
-    while (lastView?.frame.origin.x)! > max {
+    while (lastView?.frame.origin.x)! > max
+    {
       lastView?.removeFromSuperview()
       visibleView.removeLast()
       lastView = visibleView.last
@@ -127,24 +165,13 @@ class InfiniteScrollView: UIScrollView {
     
     // Remove views that have fallen off the left edge
     firstView = visibleView.first
-    while (firstView?.frame.origin.x)! < min {
+    while (firstView?.frame.origin.x)! < min
+    {
       firstView?.removeFromSuperview()
       visibleView.removeFirst()
       firstView = visibleView.first
     }
-    
   }
-  
-  
-  // Generate random color
-  func generateRandomColor() -> UIColor {
-    let hue : CGFloat = CGFloat(arc4random() % 256) / 256 // use 256 to get full range from 0.0 to 1.0
-    let saturation : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5 // from 0.5 to 1.0 to stay away from white
-    let brightness : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5 // from 0.5 to 1.0 to stay away from black
-    
-    return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
-  }
-  
 }
 
 
--- InfScrollView/ViewController.swift
+++ InfScrollView/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-  override func viewDidLoad() {
+class ViewController: UIViewController, InfiniteScrollViewDataSource
+{
+  
+  
+  let infiniteSV = InfiniteScrollView()
+  
+  override func viewDidLoad()
+  {
     super.viewDidLoad()
+    
+    infiniteSV.frame = self.view.bounds
+    infiniteSV.reloadLayout()
+    
+    self.view.addSubview(infiniteSV)
+    infiniteSV.InfiniteDelegate = self
+
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-  override func didReceiveMemoryWarning() {
+  override func didReceiveMemoryWarning()
+  {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
 
-
+  func infiniteItemForDirecection(_ direction: direction) -> UIView
+  {
+    let view = UIView()
+    view.backgroundColor = generateRandomColor()
+    return view
+  }
+  
 }
 
+// Generate random color
+func generateRandomColor() -> UIColor
+{
+  let hue : CGFloat = CGFloat(arc4random() % 256) / 256 // use 256 to get full range from 0.0 to 1.0
+  let saturation : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5 // from 0.5 to 1.0 to stay away from white
+  let brightness : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5 // from 0.5 to 1.0 to stay away from black
+  
+  return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
+}
